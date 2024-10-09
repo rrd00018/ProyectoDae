638b22a93180bc99132ac72c5f0048de45001cd2
Rafael Reinoso Díaz <rrd00018@red.ujaen.es>
2024-10-09 11:24:30 +0200

    pom cambiado para los tests y test servicioAdmin

---
+++ src/test/java/servicios/TestServiciosAdmin.java
@@ -0,0 +1,22 @@
+package servicios;
+
+import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import es.ujaen.dae.servicios.ServiciosAdmin;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(classes = ServiciosAdmin.class)
+public class TestServiciosAdmin {
+    @Autowired
+    ServiciosAdmin serviciosAdmin;
+
+    @Test
+    @DirtiesContext
+    void testNuevoSocio(){
+        var socio
+    }
+}
--- src/test/servicios/TestServicioAdmin.java
+++
@@ -1,2 +0,0 @@
-package  es.ujaen.dae.servicios;
-
