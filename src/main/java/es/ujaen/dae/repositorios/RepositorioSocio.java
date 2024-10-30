fc3e1eb4f0c09d3e9bd91ca0ef5a0233642e6fc5
Tu Nombre <tu.email@example.com>
2024-10-30 15:10:23 +0100

    Revert "Commit prueba"
    This reverts commit 4a4b854f451056be5c0d74107b059f30ccfe2f90.

--- src/main/java/es/ujaen/dae/repositorios/RepositorioSocio.java
+++ src/main/java/es/ujaen/dae/repositorios/RepositorioSocio.java
@@ -3,7 +3,6 @@ package es.ujaen.dae.repositorios;
 import es.ujaen.dae.entidades.Socio;
 import jakarta.persistence.EntityManager;
 
-
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
